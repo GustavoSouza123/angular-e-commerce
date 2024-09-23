e76fe8076700b67d14f26aaf49fc123a1fdb7ef3
Gustavo Souza <gustavoelia7@gmail.com>
2024-09-23 09:37:29 -0300

    product list passing data to child component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { products } from '../products';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductAlertsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
