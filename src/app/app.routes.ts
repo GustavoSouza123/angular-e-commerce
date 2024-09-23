4e2927a12f7e5391da291aad383dfa7172e82ae3
Gustavo Souza <gustavoelia7@gmail.com>
2024-09-23 12:27:50 -0300

    notify alert, product details with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductListComponent,
-    title: 'Home Page'
-  }
+    title: 'My Store',
+  },
+  {
+    path: 'products/:productId',
+    component: ProductDetailsComponent,
+    title: 'Product',
+  },
 ];
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { products } from '../products';
 import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, ProductAlertsComponent],
+  imports: [CommonModule, RouterLink, ProductAlertsComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
@@ -15,5 +16,9 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
